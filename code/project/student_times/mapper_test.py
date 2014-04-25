80191f02ccf62d65e7f5ae87cf046b7e25b535ee
Reiners, Paul <Paul.Reiners@Pearson.com>
2014-04-25 15:39:13 -0500

    'Yield' to The Rescue

--- code/project/student_times/mapper_test.py
+++ code/project/student_times/mapper_test.py
@@ -12,7 +12,9 @@ class StudentTimesMapperTest(unittest.TestCase):
                  '"1"\t"title"\t"tagnames"\t"1"\t"body"\t"node_type"\t"parent_id"\t"abs_parent_id"\t"2014-01-14 17:18:35.613939+00"\t"score"\t"state_string"\t"last_edited_id"\t"last_activity_by_id"\t"last_activity_at"\t"active_revision_id"\t"extra"\t"extra_ref_id"\t"extra_count"\t"marked"']
         expected = ['1\t17\t1']
 
-        result = mapper.mapper(input)
+        result = [] 
+        for output in mapper.mapper(input):
+            result.append(output)
         self.assertEqual(expected, result)
 
 if __name__ == '__main__':
