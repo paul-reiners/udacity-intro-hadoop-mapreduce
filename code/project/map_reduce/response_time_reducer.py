04481ce59b9cb72d688c3757acef701587aa7202
Paul Reiners <paul.reiners@gmail.com>
2014-05-06 13:58:08 -0500

    Delinting.

--- code/project/map_reduce/response_time_reducer.py
+++ code/project/map_reduce/response_time_reducer.py
@@ -23,27 +23,28 @@ def reducer():
     current_id = None
     # Example date: 2012-02-22 21:36:17.077627+00
     prefix_len = len('2012-02-22 21:36:17')
-    date_format =   '%Y-%m-%d %H:%M:%S'
+    date_format = '%Y-%m-%d %H:%M:%S'
     for line in reader:
         if len(line) == 4:
             the_id = line[0]
             if current_id is None or the_id != current_id:
                 if not current_id is None:
                     write_record(current_id, min_minutes_diff, writer)
-                question_time = None
-                shortest_reponse_time = None
                 current_id = the_id
 
             node_type = line[2]
             added_at = line[3]
             if node_type == "question":
-                question_added_at = datetime.strptime(added_at[:prefix_len], date_format)
+                question_added_at = \
+                    datetime.strptime(added_at[:prefix_len], date_format)
             else:
-                answer_added_at = datetime.strptime(added_at[:prefix_len], date_format)
+                answer_added_at = \
+                    datetime.strptime(added_at[:prefix_len], date_format)
                 if not question_added_at is None:
-                    dt = answer_added_at - question_added_at
-                    minutes_diff = dt.seconds / 60
-                    if min_minutes_diff is None or minutes_diff < min_minutes_diff:
+                    delta = answer_added_at - question_added_at
+                    minutes_diff = delta.seconds / 60
+                    if min_minutes_diff is None or \
+                        minutes_diff < min_minutes_diff:
                         min_minutes_diff = minutes_diff
     write_record(current_id, min_minutes_diff, writer)
 
